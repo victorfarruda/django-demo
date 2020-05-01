9d02b66bcc502fa8ca5b78d822b6ed4413d2fed4
Victor Arruda <victorf_arruda@outlook.com>
2020-04-30 21:57:11 -0300

    template tag url and reverse

--- app/base/tests/test_home.py
+++ app/base/tests/test_home.py
@@ -1,13 +1,24 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
 
 from app.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    response = client.get('/')
-    assert response.status_code == 200
+@pytest.fixture
+def resp(client: Client):
+    home_url = reverse('home')
+    response = client.get(home_url)
+    return response
 
 
-def test_title(client: Client):
-    response = client.get('/')
-    assert_contains(response, '<title>Aprendizado</title>')
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Site de Estudo</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("home")}">Site de Estudo</a>')
